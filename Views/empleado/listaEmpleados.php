d9d03549972fed0a44cc3f17574b4872b00e148c
NitaiFraire <fernandezfrairenitai@hotmail.com>
2019-03-27 20:58:40 -0600

    directorio para vistas de empleado con vista de listado de empleados totales

---
+++ Views/empleado/listaEmpleados.php
@@ -0,0 +1,31 @@
+<?php require_once 'Views/layout/header.php'; ?>
+<h1 class="text-center">Empleados</h1>
+<table class="table table-striped">
+  <thead>
+    <tr>
+      <th scope="col">ID</th>
+      <th scope="col">Nombre</th>
+      <th scope="col">Paterno</th>
+      <th scope="col">Materno</th>
+      <th scope="col">Email</th>
+      <th scope="col">Telefono</th>
+      <th scope="col">Colonia</th>
+      <th scope="col">Domicilio</th>
+    </tr>
+  </thead>
+  <tbody>
+  <?php while($empleado = $empleados->fetch_object()):?>
+    <tr>
+      <th scope="row"><?=$empleado->idEmpleado?></th>
+      <td><?=$empleado->nombre?></td>
+      <td><?=$empleado->paterno?></td>
+      <td><?=$empleado->materno?></td>
+      <td><?=$empleado->email?></td>
+      <td><?=$empleado->telefono?></td>
+      <td><?=$empleado->idColonia?></td>
+      <td><?=$empleado->domicilio?></td>
+    </tr>
+    <?php endwhile;?>
+  </tbody>
+</table>
+<?php require_once 'Views/layout/footer.php'; ?>
