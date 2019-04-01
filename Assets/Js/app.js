e8e8fa8d8bb6c3b834d0cd69eb75d34f716af9e5
NitaiFraire <fernandezfrairenitai@hotmail.com>
2019-04-01 11:32:21 -0600

    peticiones asincronas a la base de datos para seleccionar estado, ciudad, colonia

--- Assets/Js/app.js
+++ Assets/Js/app.js
@@ -19,3 +19,71 @@ menuIzquierdo.addEventListener('click', (e) => {
         flechaIzq.style.display = 'block';
     }
 });
+
+
+$(document).ready(function(){
+
+    $("#pais").change(function(){
+    
+        var id=$(this).val();
+        var dataString = 'idPais='+ id;
+        var direccion = 'http://localhost/Actividades/Paqueteria/Ubicacion/getEstados&' + dataString;
+        console.log(direccion);
+
+        $("#estado").find('option').remove();
+        $("#ciudad").find('option').remove();
+        $("#colonia").find('option').remove();
+        
+        $.ajax({
+        
+            type: "GET",
+            url: direccion,
+            /* data: dataString, */
+            cache: false,
+            success: function(html){
+                
+                $("#estado").html(html);
+            }
+        });
+    });
+
+    $("#estado").change(function(){
+    
+        var id=$(this).val();
+        var dataString = 'idEstado='+ id;
+        var direccion = 'http://localhost/Actividades/Paqueteria/Ubicacion/getCiudades&' + dataString;
+
+        $.ajax({
+
+            type: "GET",
+            url: direccion,
+            /* data: dataString */
+            cache: false,
+            success: function(html){
+            
+                $("#ciudad").html(html);
+            } 
+        });
+    });
+
+    $("#ciudad").change(function(){
+    
+        var id=$(this).val();
+        var dataString = 'idCiudad='+ id;
+        var direccion = 'http://localhost/Actividades/Paqueteria/Ubicacion/getColonias&' + dataString;
+        console.log(direccion);
+
+        $.ajax({
+
+            type: "GET",
+            url: direccion,
+            /* data: dataString */
+            cache: false,
+            success: function(html){
+            
+                $("#colonia").html(html);
+            } 
+        });
+    });
+
+});
