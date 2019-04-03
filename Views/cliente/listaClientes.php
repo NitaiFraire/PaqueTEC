80708c84155386b1cf9371fad73f423f785ecb0a
NitaiFraire <fernandezfrairenitai@hotmail.com>
2019-04-03 10:28:48 -0600

    añadido campo nombrePais, nombreEstado, nombreCiudad, nombreColonia a listado empleados

---
+++ Views/cliente/listaClientes.php
@@ -0,0 +1,37 @@
+<?php require_once 'Views/layout/header.php'; ?>
+<h1 class="text-center">Clientes</h1>
+<table class="table table-striped">
+  <thead>
+    <tr>
+      <th scope="col">ID</th>
+      <th scope="col">Nombre</th>
+      <th scope="col">Paterno</th>
+      <th scope="col">Materno</th>
+      <th scope="col">Email</th>
+      <th scope="col">Telefono</th>
+      <th scope="col">Colonia</th>
+      <th scope="col">Ciudad</th>
+      <th scope="col">Estado</th>
+      <th scope="col">Pais</th>
+      <th scope="col">Domicilio</th>
+    </tr>
+  </thead>
+  <tbody>
+  <?php while($cliente = $clientes->fetch_object()):?>
+    <tr>
+      <th scope="row"><?=$cliente->idCliente?></th>
+      <td><?=$cliente->nombre?></td>
+      <td><?=$cliente->paterno?></td>
+      <td><?=$cliente->materno?></td>
+      <td><?=$cliente->email?></td>
+      <td><?=$cliente->telefono?></td>
+      <td><?=$cliente->idColonia?></td>
+      <td><?=$cliente->idCiudad?></td>
+      <td><?=$cliente->idEstado?></td>
+      <td><?=$cliente->idPais?></td>
+      <td><?=$cliente->domicilio?></td>
+    </tr>
+    <?php endwhile;?>
+  </tbody>
+</table>
+<?php require_once 'Views/layout/footer.php'; ?>
