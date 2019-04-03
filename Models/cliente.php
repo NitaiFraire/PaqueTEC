56727038e49fe390f0ef3737f10bd399f39d4b84
NitaiFraire <fernandezfrairenitai@hotmail.com>
2019-04-03 10:25:07 -0600

    registro y listado de clientes

--- Models/cliente.php
+++ Models/cliente.php
@@ -23,6 +23,49 @@ class Cliente extends Persona{
 
     }
 
+    /* Funciones de modelo */
+
+    public function register(){
+        
+        $sql = "INSERT INTO clientes VALUES('{$this->getIdColonia()}',
+                                            '{$this->getIdCiudad()}',
+                                            '{$this->getIdEstado()}',
+                                            '{$this->getIdPais()}',
+                                            NULL,
+                                            '{$this->getNombre()}',
+                                            '{$this->getPaterno()}',
+                                            '{$this->getMaterno()}',
+                                            '{$this->getEdad()}',
+                                            '{$this->getRfc()}',
+                                            '{$this->getCurp()}',
+                                            '{$this->getGenero()}',
+                                            '{$this->getEmail()}',
+                                            '{$this->getTelefono()}',
+                                            '{$this->getDomicilio()}')";
+        $save = $this->db->query($sql);
+
+        $result = false;
+
+        if($save){
+
+            $result = true;
+
+        }
+
+        return $result;
+    }
+
+    public function getClientes(){
+        
+        $sql = "SELECT * FROM clientes ORDER BY idCliente DESC";
+
+        $clientes = $this->db->query($sql);
+
+        return $clientes;
+    }
+
+    /* /Funciones de modelo */
+
 }
 
 
