16a70b27392a5d7873d60eac6bef1ea130e6c805
NitaiFraire <fernandezfrairenitai@hotmail.com>
2019-04-05 22:51:15 -0600

    listado de todos los paquetes

--- Views/paquete/listadoPaquetes.php
+++ Views/paquete/listadoPaquetes.php
@@ -1,4 +1,43 @@
 <?php require_once 'Views/layout/header.php'; ?>
-<h1>Funcionando</h1>
-
+<h1 class="text-center">Paquetes</h1>
+<table class="table table-striped">
+  <thead>
+    <tr>
+      <th scope="col">ID</th>
+      <th scope="col">Cliente</th>
+      <th scope="col">Empleado</th>
+      <th scope="col">Fecha de alta</th>
+      <th scope="col">Fecha de envio</th>
+      <th scope="col">Fecha de entrega</th>
+      <th scope="col">Hora de entrega</th>
+      <th scope="col">Contenido</th>
+      <th scope="col">Observaciones</th>
+      <th scope="col">Estado</th>
+    </tr>
+  </thead>
+  <tbody>
+  <?php while($paquete = $paquetes->fetch_object()):?>
+    <tr>
+      <th scope="row"><?=$paquete->idPaquete?></th>
+      <td><?=$paquete->cliente?></td>
+      <td><?=$paquete->empleado?></td>
+      <td><?=$paquete->diaAlta?></td>
+      <td><?=$paquete->fechaEnvio?></td>
+      <td><?=$paquete->fechaEntrega?></td>
+      <td><?=$paquete->horaEntrega?></td>
+      <td><?=$paquete->contenido?></td>
+      <td><?=$paquete->observaciones?></td>
+      <td>
+          <?php if($paquete->estado == 0):?>
+            registrado
+          <?php elseif($paquete->estado == 1):?>
+            Enviado
+          <?php else: ?>
+            Entregado
+          <?php endif;?>
+      </td>
+    </tr>
+    <?php endwhile;?>
+  </tbody>
+</table>
 <?php require_once 'Views/layout/footer.php'; ?>
