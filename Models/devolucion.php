3beb517dcd3a02db7086ae314cbd257e8e95c7e6
NitaiFraire <fernandezfrairenitai@hotmail.com>
2019-04-08 22:28:19 -0500

    funciones de modelo

--- Models/devolucion.php
+++ Models/devolucion.php
@@ -60,6 +60,59 @@ class Devolucion{
 
         $this->motivoDevolucion = $motivoDevolucion;
     }
+
+    /* Funciones de modelo */
+
+    public function getPaquetesADevolver(){
+        
+        $sql = "SELECT * FROM paquetes WHERE estado = 3 AND
+                                             idPaquete = '{$this->getIdPaquete()}'";
+
+        $paquetes = $this->db->query($sql);
+
+        return $paquetes->fetch_object();
+    }
+
+    public function register(){
+        
+        $sql = "INSERT INTO devoluciones VALUES('{$this->getIdPaquete()}',    
+                                                NULL,
+                                                '{$this->getFechaDevolucion()}',
+                                                '{$this->getMotivoDevolucion()}')";
+        
+        $result = false;
+
+        $save = $this->db->query($sql);
+
+        if($save){
+
+            $result = true;
+        }
+
+        return $result;
+    }
+
+    public function getAll(){
+        
+        $sql = "SELECT  dev.*,
+                        paq.diaAlta, paq.fechaEnvio,
+                        CONCAT(emp.nombre, ' ', emp.paterno) AS empleado,
+                        CONCAT(cli.nombre, ' ', cli.paterno) AS cliente
+                
+                FROM    devoluciones dev, paquetes paq, empleados emp, clientes cli 
+
+                WHERE   dev.idPaquete = paq.idPaquete AND
+                        paq.idCliente = cli.idCliente AND
+                        paq.idEmpleado = emp.idEmpleado
+                
+                ORDER BY idDevolucion DESC;";
+
+        $devoluciones = $this->db->query($sql);
+
+        return $devoluciones;
+    }
+
+    /* /Funciones de modelo */
 }
 
 ?>
