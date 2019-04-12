3db2b14a3bf115389d20d3e6925dba06cbcc86e9
NitaiFraire <fernandezfrairenitai@hotmail.com>
2019-04-11 19:26:23 -0500

    edicion de pantalla de error

--- Views/error.php
+++ Views/error.php
@@ -1,4 +1,8 @@
-<?php require_once 'Views/layout/header.php'?>
+<?php if(isset($_SESSION['identified'])): ?>
+    <?php require_once 'Views/layout/header.php'?>
+<?php else: ?>
+    <?php require_once 'Views/layout/headerError.php'?>
+<?php endif; ?>
 <div class="container cError">
     <div class="row justify-content-center mt-5">
         <div class="col-md-8 text-center">
